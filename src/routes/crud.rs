f63fbd7c4cdd9a4999feaa4c80e8d29621780cf6
fcruxen <fcruxen@gmail.com>
2023-02-27 21:49:37 -0300

    all methods working

--- src/routes/crud.rs
+++ src/routes/crud.rs
@@ -1,6 +1,8 @@
+use std::fmt::Debug;
+use std::str::FromStr;
 use actix_web::{HttpResponse, Scope, web};
 use actix_web::http::StatusCode;
-use sea_orm::{ActiveModelBehavior, ActiveModelTrait, ColumnTrait, DatabaseConnection, DbErr, EntityOrSelect, EntityTrait, IntoActiveModel, ModelTrait, PrimaryKeyTrait, Value};
+use sea_orm::{ActiveModelBehavior, ActiveModelTrait, ColumnTrait, ColumnTypeTrait, DatabaseConnection, DbErr, EntityOrSelect, EntityTrait, IntoActiveModel, ModelTrait, PrimaryKeyTrait, Value};
 use serde::{Deserialize, Serialize};
 use async_trait::async_trait;
 use sea_orm::ActiveValue::Set;
@@ -8,23 +10,50 @@ use sea_orm::sea_query::ColumnRef::Column;
 use sea_orm::sea_query::ValueTuple;
 use serde_json::json;
 use json_value_merge::Merge;
+use sea_orm::sea_query::extension::postgres::IntoTypeRef;
 use serde_json::Value as JsonValue;
+
 
 #[async_trait]
 pub trait CrudRoutes<T, M, N>
     where
         T: EntityTrait,
         T::Model: Serialize,
-        N: IntoActiveModel<M> + Serialize + Sync + Send + 'static ,
+        N: IntoActiveModel<M> + Serialize + Sync + Send + 'static + Clone,
         M: ActiveModelTrait + ActiveModelBehavior + Send + Sync + From<<T as EntityTrait>::Model>,
         <<M as ActiveModelTrait>::Entity as EntityTrait>::Model: IntoActiveModel<M>,
         <<M as ActiveModelTrait>::Entity as EntityTrait>::Model: for<'de> Deserialize<'de>,
         <<T as EntityTrait>::PrimaryKey as PrimaryKeyTrait>::ValueType: From<i32>,
-        <T as EntityTrait>::Model: Sync
+        <T as EntityTrait>::Model: Sync,
+        <<<M as ActiveModelTrait>::Entity as EntityTrait>::Column as FromStr>::Err: Debug
 {
     fn entity() -> T;
     fn model() -> M;
+    fn primary_column() -> <<M as ActiveModelTrait>::Entity as EntityTrait>::Column {
+        <<M as ActiveModelTrait>::Entity as EntityTrait>::Column::from_str("id").unwrap()
+    }
     fn export_routes() -> Scope;
+
+    async fn update(
+        path: web::Path<(i32,)>,
+        record: web::Json<N>,
+        data: web::Data<DatabaseConnection>,
+    ) -> HttpResponse {
+        let id: i32 = path.into_inner().0;
+        match T::find_by_id(id).one(data.as_ref()).await {
+            Ok(_) => {
+                let mut updated_record = record.into_inner().clone().into_active_model();
+                updated_record.set(Self::primary_column(), Value::Int(Some(id)));
+                match updated_record.save(data.as_ref()).await {
+                    Ok(_) => HttpResponse::Ok().status(StatusCode::ACCEPTED).json(""),
+                    Err(err) => Self::default_err(err)
+                }
+            },
+            Err(err) => {
+                return Self::default_err(err);
+            }
+        }
+    }
 
     fn default_err(err: DbErr) -> HttpResponse {
         HttpResponse::BadRequest()
@@ -51,39 +80,7 @@ pub trait CrudRoutes<T, M, N>
         }
     }
 
-    async fn update(
-        path: web::Path<(i32,)>,
-        record: web::Json<N>,
-        data: web::Data<DatabaseConnection>,
-    ) -> HttpResponse {
-        let id: i32 = path.into_inner().0;
-        let mut existing_record: M = match T::find_by_id(id).one(data.as_ref()).await {
-            Ok(Some(record)) => record.into(),
-            Ok(None) => {
-                return Self::default_err(DbErr::Custom(format!("Could not find id: {}", id)));
-            }
-            Err(err) => {
-                return Self::default_err(err);
-            }
-        };
-        log::debug!("Record found, changing it");
-        let mut new_record = record.into_inner().into_active_model();
-        let mut merged_data = JsonValue::new_object();
-        merged_data.merge(existing_record.to_serialize()).unwrap();
-        merged_data.merge(new_record.to_serialize()).unwrap();
-        new_record = M::from(merged_data).unwrap();
-        let j = json!({"id": id});
-        match  update_record {
-            Ok(..) => {
-                match new_record.update(data.as_ref()).await {
-                    Ok(_) => HttpResponse::Ok().status(StatusCode::ACCEPTED).body(""),
-                    Err(err) => Self::default_err(err),
-                }
-            }
-            Err(err) => Self::default_err(err)
-        }
 
-    }
 
 
     async fn create(record: web::Json<N>, data: web::Data<DatabaseConnection>) -> HttpResponse {
